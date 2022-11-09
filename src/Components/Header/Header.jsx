56190d56ef9f6bee8bb0c0b7125b35b18100f6c4
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-11-09 09:46:47 +0600

    first commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import CV from "../../../assets/cv.pdf";
 
 import "./Header.css";
 const Header = () => {
   return (
-    <Header>
-      <div className="container header__container">
+    <>
+      <div>
         <h5>Hello I'm</h5>
         <h1>Mehedi Hasan Himel</h1>
-        <h5 className="text-light">Front-End Web Developer.</h5>
-        <a href={CV} download>
-          Download CV
-        </a>
-        <a href="#contact"> Let's Talk. </a>
+        <h5>Front-End Web Developer.</h5>
       </div>
-    </Header>
+    </>
   );
 };
 
