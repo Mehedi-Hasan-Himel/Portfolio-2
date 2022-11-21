8063f364c5d239a875a7e91d5c4fe8e6694f6218
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-11-21 08:05:47 +0600

    services

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -13,42 +13,42 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>HTML</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>CSS</h4>
                 <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>SASS</h4>
                 <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>BootStrap</h4>
                 <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>TailwindCSS</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>ReactJS</h4>
                 <small className="text-light">Experienced</small>
@@ -63,21 +63,21 @@ const Experience = () => {
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>NodeJS</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>ExpressJS</h4>
                 <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>MongoDB</h4>
                 <small className="text-light">Intermediate</small>
@@ -92,14 +92,14 @@ const Experience = () => {
           <h3>Full Stack Library </h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>NextJS</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>Redux</h4>
                 <small className="text-light">Intermediate</small>
@@ -114,21 +114,21 @@ const Experience = () => {
           <h3>Programming Languages</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>JavaScript</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>TypeScript</h4>
                 <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill></BsPatchCheckFill>
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
               <div>
                 <h4>Solidity</h4>
                 <small className="text-light">Beginner </small>
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,8 +1,121 @@
-import React from 'react';
-import "./Services.css"
+import React from "react";
+import "./Services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
-   return <section id="services">services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        {/* UI/UX Start */}
+        <article className="services">
+          <div className="services__head">
+            <h3>UI / UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* UI/UX End */}
+
+        {/* Web Development Start */}
+        <article className="services">
+          <div className="services__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* Web Development End */}
+
+        {/* Content Creation Start */}
+        <article className="services">
+          <div className="services__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* Content Creation End */}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
