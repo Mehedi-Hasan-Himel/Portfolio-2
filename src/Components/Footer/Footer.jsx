ed370783471862eb122e8ea164e40c85c4131c1b
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-11-22 10:41:09 +0600

    done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,30 +7,30 @@ import { IoLogoTwitter } from "react-icons/io";
 const Footer = () => {
   return (
     <footer id="footer">
-      <a href="" className="footer__logo">
+      <h3  className="footer__logo">
         Mehedi Hasan Himel
-      </a>
+      </h3>
       <ul className="permalinks">
         <li>
-          <a href="home">Home</a>
+          <a href="#home">Home</a>
         </li>
         <li>
-          <a href="about">About</a>
+          <a href="#about">About</a>
         </li>
         <li>
-          <a href="experience">Experience</a>
+          <a href="#experience">Experience</a>
         </li>
         <li>
-          <a href="services">Services</a>
+          <a href="#services">Services</a>
         </li>
         <li>
-          <a href="portfolio">Portfolio</a>
+          <a href="#portfolio">Portfolio</a>
         </li>
         <li>
-          <a href="testimonials">Testimonials</a>
+          <a href="#testimonials">Testimonials</a>
         </li>
         <li>
-          <a href="contact">Contact</a>
+          <a href="#contact">Contact</a>
         </li>
       </ul>
       <div className="footer__socials">
