c6921a5f3389a35f1fda6d1fbe4ebb4ce4216d1c
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-11-22 10:29:26 +0600

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./Footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
-  return <section id="footer">footer</section>;
+  return (
+    <footer id="footer">
+      <a href="" className="footer__logo">
+        Mehedi Hasan Himel
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="home">Home</a>
+        </li>
+        <li>
+          <a href="about">About</a>
+        </li>
+        <li>
+          <a href="experience">Experience</a>
+        </li>
+        <li>
+          <a href="services">Services</a>
+        </li>
+        <li>
+          <a href="portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF></FaFacebookF>
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram></FiInstagram>
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter></IoLogoTwitter>
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mehedi Hasan Himel. All rights reserved.</small>
+      </div>
+
+    </footer>
+  );
 };
 
 export default Footer;
